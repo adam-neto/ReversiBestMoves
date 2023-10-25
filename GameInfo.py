5fc2c25910d4ee8e9cea57d9afb72c32a45ad664
adam-neto <132019921+adam-neto@users.noreply.github.com>
2023-10-25 10:44:54 -0400

    Update GameInfo.py

--- GameInfo.py
+++ GameInfo.py
@@ -10,15 +10,25 @@ BOARD_HEIGHT = 8
 
 class GameState:
 
+    # initialize game state
     def __init__(self):
         # initialize game board
-        w = [[False for j in range(BOARD_WIDTH)] for i in range(BOARD_HEIGHT)]
-        b = [[False for j in range(BOARD_WIDTH)] for i in range(BOARD_HEIGHT)]
+        self.w = [[False for j in range(BOARD_WIDTH)] for i in range(BOARD_HEIGHT)]
+        self.b = [[False for j in range(BOARD_WIDTH)] for i in range(BOARD_HEIGHT)]
 
         # initial white positions
-        w[3][3] = True
-        w[4][4] = True
+        self.w[3][3] = True
+        self.w[4][4] = True
 
         # initial black positions
-        b[4][3] = True
-        b[3][4] = True
+        self.b[4][3] = True
+        self.b[3][4] = True
+
+    def place_piece(self, x, y, player):
+        # check space is empty
+        if not self.w[x][y]:
+            if not self.b[x][y]:
+                if player == "white":
+                    self.w[x][y] = True
+                else:
+                    self.b[x][y] = True
