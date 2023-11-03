7983ad76c978424e98e58f57f595f4c50747792f
adam-neto <132019921+adam-neto@users.noreply.github.com>
2023-11-02 21:36:39 -0400

    Update GameDriver.py
    now shows modelling information about valid moves, how many pieces they capture.
    This message is only shown if an invalid move position is suggested (it acts as a hint)

--- GameDriver.py
+++ GameDriver.py
@@ -2,7 +2,7 @@
 TEST FILE FOR DRIVING GAME STATE
 
 Author: Adam Neto
-Updated: 10/26/23
+Updated: 11/2/23
 """
 import GameInfo
 
@@ -11,9 +11,11 @@ myGame.draw_board()
 
 playing = True
 while playing:
+    print()
     x = int(input(myGame.player + " player, next x: "))
     y = int(input(myGame.player + " player, next y: "))
     print()
     if not myGame.place_piece(x, y):
         print("Invalid move")
+        myGame.scan_board()
     myGame.draw_board()
